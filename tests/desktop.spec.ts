4dfcc58adb4ac7d3ce7d381dc77feb0ad4a68100
Michal N. <michal.nerowski@gmail.com>
2026-02-03 21:06:22 +0100

    AAA implementation

--- tests/desktop.spec.ts
+++ tests/desktop.spec.ts
@@ -1,22 +1,32 @@
-import {test, expect} from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test.describe('Desktop tests', () => {
-    
-  test('make a transaction', async ({ page }) => {
-    await page.goto('https://demo-bank.vercel.app/');
-    await page.getByTestId('login-input').fill('tester84');
-    await page.getByTestId('password-input').fill('12345678');
-    await page.getByTestId('login-button').click();
 
-    await page.locator('#widget_1_transfer_receiver').selectOption('1');
-    await page.locator('#widget_1_transfer_amount').fill('120');
-    await page.locator('#widget_1_transfer_title').fill('Refund');
+  test('make a transaction', async ({ page }) => {
+    //Arrange
+    const url = 'https://demo-bank.vercel.app/';
+    const userId = 'tester84';
+    const userPassword = '12345678';;
+
+    const receiverId = '1';
+    const transferAmount = '120';
+    const transferTitle = 'Refund';
+
+    //Act
+    await page.goto(url);
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(userPassword);
+    await page.getByTestId('login-button').click();
+    await page.locator('#widget_1_transfer_receiver').selectOption(receiverId);
+    await page.locator('#widget_1_transfer_amount').fill(transferAmount);
+    await page.locator('#widget_1_transfer_title').fill(transferTitle);
     await page.locator('#execute_btn').click();
     //await page.getByRole('button', { name: 'wykonaj' }).click(); też jest poprawne
     await page.getByTestId('close-button').click();
     //await page.getByRole('link', { name: 'Przelew wykonany! Jan' }).click();
 
-    await expect(page.locator('#show_messages')).toHaveText('Przelew wykonany! Jan Demobankowy - 120,00PLN - Refund');
+    //Assert
+    await expect(page.locator('#show_messages')).toHaveText(`Przelew wykonany! Jan Demobankowy - ${transferAmount},00PLN - ${transferTitle}`);
   });
 
   test('successful phone top-up', async ({ page }) => {
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,12 +3,20 @@ import { test, expect } from '@playwright/test';
 test.describe('User login to Demobank', () => {
 
   test('successful login with the correct credentials', async ({ page }) => {
-    await page.goto('https://demo-bank.vercel.app/');
-    await page.getByTestId('login-input').fill('tester84');
-    await page.getByTestId('password-input').fill('12345678');
+    //Arrange
+    const url = 'https://demo-bank.vercel.app/';
+    const userId = 'tester84';
+    const userPassword = '12345678';
+    const expectedUserName = 'Jan Demobankowy';
+    
+    //Act
+    await page.goto(url);
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
 
-    await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy');
+    //Assert
+    await expect(page.getByTestId('user-name')).toHaveText(expectedUserName);
   });
 
   test('unsuccessful login with too short user name', async ({ page }) => {
