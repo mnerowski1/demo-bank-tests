fad0952764105160ef70c842545b5f3d7e6308b5
Michal N. <michal.nerowski@gmail.com>
2026-02-11 23:10:23 +0100

    main page added to config

--- tests/desktop.spec.ts
+++ tests/desktop.spec.ts
@@ -2,11 +2,10 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Desktop tests', () => {
   test.beforeEach(async ({ page }) => {
-    const url = 'https://demo-bank.vercel.app/';
     const userId = 'tester84';
     const userPassword = '12345678';
 
-    await page.goto(url);
+    await page.goto('/');
 
     await page.getByTestId('login-input').fill(userId);
     await page.getByTestId('password-input').fill(userPassword);
