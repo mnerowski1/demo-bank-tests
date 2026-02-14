fe92e0a945cc9568d1562c83fac5a89daec0fd2d
Michal N. <michal.nerowski@gmail.com>
2026-02-14 20:09:54 +0100

    balance test adn scripts

--- tests/desktop.spec.ts
+++ tests/desktop.spec.ts
@@ -48,4 +48,22 @@ test.describe('Desktop tests', () => {
     //Assert
     await expect(page.locator('#show_messages')).toHaveText(expectedMessage);
   });
+
+    test('correct balance after successful phone top-up', async ({ page }) => {
+    //Arrange
+    const receiverPhone = '502 xxx xxx';
+    const transferAmount = '50';
+    const initialBalance = await page.locator('#money_value').innerText();
+    const expectedBalance = Number(initialBalance) - Number(transferAmount);
+
+    //Act
+    await page.locator('#widget_1_topup_receiver').selectOption(receiverPhone);
+    await page.locator('#widget_1_topup_amount').fill(transferAmount);
+    await page.locator('#uniform-widget_1_topup_agreement').click();
+    await page.locator('#execute_phone_btn').click();
+    await page.getByTestId('close-button').click();
+
+    //Assert
+    await expect(page.locator('#money_value')).toHaveText(`${expectedBalance}`);
+  });
 });
